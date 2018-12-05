770666e56620bb4121582a7be5d8468473fa0e73
RayKilburg <rkilburg42@gmail.com>
2018-12-04 20:57:09 -0500

    test

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,9 +6,9 @@ import Image from '../components/image'
 
 const IndexPage = () => (
   <Layout>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>test</h1>
+    <p>test.</p>
+    <p>test.</p>
     <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
       <Image />
     </div>
